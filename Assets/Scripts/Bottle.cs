eac9281539802459f3683e71cf44a06c332230a8
TillJWalter <tillusjonas@gmail.com>
2022-11-04 22:13:03 +0100

    Bottle Script

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -2,17 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Bottle Script
+ */
 public class Bottle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private BottleType _bottleType;
         
+    private bool _pickedUp; // if the bottle was picked up by the player or still lays on the ground
+
+    public enum BottleType
+    {
+        red,
+        blue,
+        yellow
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        _pickedUp = false;
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        #region BottleOnGround
+
+        if (!_pickedUp) // Bottle has not been collected by the player yet
+        {
+            if (other.transform.CompareTag("Player"))
+            {
+                // Player needs to collect bottle
+                // TODO other.GetComponent<PlayerController>().Collect(this);
+
+                // Collider and Renderer get disabled on pickup but the item GameObject is not removed
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<Collider2D>().enabled = false;
+                transform.parent = other.transform;
+                transform.localPosition = Vector3.zero;
+                _pickedUp = true;
+            }
+        }
+
+        #endregion
+
+        #region BottleThrown
+
+        else // Bottle was thrown by the player
+        {
+            if (other.transform.CompareTag("Wall"))
+            {
+                // Bottle hit another wall
+                // TODO Add impact on wall
+                Destroy(this.gameObject);
+            }
+
+            if (other.transform.CompareTag("Enemy"))
+            {
+                // Bottle hit an enemy
+                // TODO Add impact on enemy
+                Destroy(this.gameObject);
+            }
+        }
+
+        #endregion
+    }
+
+    /**
+     * Needs to called when the bottle is thrown
+     */
+    void OnThrow()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<Collider2D>().enabled = true;
+    }
+
+    public BottleType GetBottleType()
+    {
+        return _bottleType;
+    }
+    
 }
