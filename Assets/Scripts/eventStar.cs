03289bcdb3bea898e611deb5b06ae85b8ae48fd7
DasApfelkuchem <benniediek@gmail.com>
2022-11-05 22:09:56 +0100

    s

--- Assets/Scripts/eventStar.cs
+++ Assets/Scripts/eventStar.cs
@@ -15,6 +15,12 @@ public class eventStar : MonoBehaviour
         yield return new WaitForSeconds(20f);
         sound.Play();
     }
+    IEnumerator con()
+    {
+        StartCoroutine(con());
+        yield return new WaitForSeconds(60f);
+        sound.Play();
+    }
     // Update is called once per frame
     void Update()
     {
