0f17bc2050b41ea27ee7ab0a8fac2d2b18684f5e
TillJWalter <tillusjonas@gmail.com>
2022-11-05 14:47:46 +0100

    Bottle Throw

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject bottleRoyal;
+    Status.Type wallStatus;
+    SpriteRenderer wallSprite;
+
+    private void Start()
     {
-        
+        wallSprite = GetComponent<SpriteRenderer>();
+        wallStatus = Status.Type.empty;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Bottle"))
+        {
+            bottleRoyal = collision.gameObject;
+            if (wallStatus == Status.Type.empty)
+            {
+                ChangeWallStatus(bottleRoyal.GetComponent<Bottle>().GetBottleType());
+            }
+            else
+            {
+                Destroy(this.gameObject); // or something different
+            }
+        }
+    }
+
+    void ChangeWallStatus(Status.Type newStatus)
+    {
+        wallStatus = newStatus;
+        if(wallStatus == Status.Type.blue)
+        {
+            wallSprite.color = Color.blue;
+        }
+        if (wallStatus == Status.Type.red)
+        {
+            wallSprite.color = Color.red;
+        }
+        if (wallStatus == Status.Type.yellow)
+        {
+            wallSprite.color = Color.yellow;
+        }
     }
 }
