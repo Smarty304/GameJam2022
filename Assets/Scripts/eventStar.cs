15f79d327a2e07d214735353abbadd8da4ae67ee
DasApfelkuchem <benniediek@gmail.com>
2022-11-05 17:37:46 +0100

    deko

---
+++ Assets/Scripts/eventStar.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eventStar : MonoBehaviour
+{
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startevent()); 
+    }
+    IEnumerator startevent()
+    {
+        yield return new WaitForSeconds(20f);
+        sound.Play();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
