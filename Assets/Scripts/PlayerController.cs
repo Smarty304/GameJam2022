bd25d5e8fa732eb6594c802c7e67f22439d321b1
TillJWalter <tillusjonas@gmail.com>
2022-11-05 01:39:58 +0100

    new PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,86 +7,129 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private InputActionReference _movementControl;
-    [SerializeField] private InputActionReference _dig;
+    [SerializeField] private InputActionReference _throw;
     [SerializeField] private float _playerSpeed = 2.0f;
     [SerializeField] private float _movementVelocity = 3.0f;
-    [SerializeField] private Transform _diggingArea;
-    [SerializeField] private float _digingRange = 0.5f;
-    [SerializeField] private LayerMask _earthBlockLaya;
-    private Animator _myAnimator;
     private Rigidbody2D _myRigidbody;
-    private SpriteRenderer _myspriteRenderer;
 
+    public float HorizontalMoveSpeed, VerticalMoveSpeed;
+    public float MaxJumpTime = 0.15f;
+    public float AdditionalJumpForce = 10;
+    public const float GRAVITY_SCALE = 5;
+    public const float FALLING_GRAVITY_SCALE = 8f;
+    public const float MIN_FORCE = 5;
+    public const float MAX_FORCE_NORMAL = 6;
+
+    private bool _jumping;
+    private bool _touchesGround;
+    private float _currentJumpTime;
 
     private void OnEnable()
     {
         _movementControl.action.Enable();
-        _dig.action.Enable();
+        _throw.action.Enable();
     }
 
     private void OnDisable()
     {
         _movementControl.action.Disable();
-        _dig.action.Disable();
+        _throw.action.Disable();
     }
     void Start()
     {
         _myRigidbody = GetComponent<Rigidbody2D>();
-        _myAnimator = GetComponent<Animator>();
-        _myspriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void OnDig(InputValue value)
-    {
-        Debug.Log("hi");
-        //digging animation
-        //block detection
-        Collider2D[] hitGround = Physics2D.OverlapCircleAll(_diggingArea.position, _digingRange, _earthBlockLaya);
-
-
-        //destroy block
-        foreach(Collider2D _ground in hitGround)
-        {
-            _ground.GetComponent<blockemyControl>().TakeDamage();
-            Debug.Log("Diggi Diggi Hole");
-            _myAnimator.SetBool("isDigging", true);
-        }
-
-    }
     void Update()
     {
-        Vector3 movement = _movementControl.action.ReadValue<Vector2>() * _movementVelocity;
-        transform.position += movement * Time.deltaTime;
+    //    Vector3 movement = _movementControl.action.ReadValue<Vector2>() * _movementVelocity;
+    //    transform.position += movement * Time.deltaTime;
+
+    if (_movementControl.action.ReadValue<Vector2>().y > 0)
+    {
+        // Jumps
+        if (_jumping)
+        {
+            _currentJumpTime += Time.deltaTime;
+        }
+    }
+    else
+    {
+        if (_jumping)
+        {
+            _jumping = false;
+            _currentJumpTime = 0;
+        }
+    }
         
-        FlipSprite();
     }
 
-
-
-    void FlipSprite()
+    private void FixedUpdate()
     {
-        Debug.Log(_movementControl.action.ReadValue<Vector2>());
-        if(_movementControl.action.ReadValue<Vector2>().x < 0f) 
+        Vector2 movement = _movementControl.action.ReadValue<Vector2>();
+
+        if (movement.x < 0)
         {
-            _myAnimator.SetBool("isDigging", false);
-            _myspriteRenderer.flipX = true;
-            _myAnimator.SetBool("isRunning", true);
-        }
+            _myRigidbody.AddForce(new Vector2(-HorizontalMoveSpeed * Time.fixedDeltaTime, 0), ForceMode2D.Force);
+
+            if (_myRigidbody.velocity.x > -MIN_FORCE)
+            {
+                _myRigidbody.velocity = new Vector2(-MIN_FORCE, _myRigidbody.velocity.y);
+            }
+            else if (_myRigidbody.velocity.x < -MAX_FORCE_NORMAL)
+            {
+                _myRigidbody.velocity = new Vector2(-MAX_FORCE_NORMAL, _myRigidbody.velocity.y);
+
+            }
             
-        
-        else if(_movementControl.action.ReadValue<Vector2>().x > 0f) 
+        }
+        else if (movement.x > 0)
         {
-            _myAnimator.SetBool("isDigging", false);
-            _myspriteRenderer.flipX = false;
-            _myAnimator.SetBool("isRunning", true);
+            _myRigidbody.AddForce(new Vector2(HorizontalMoveSpeed * Time.fixedDeltaTime, 0), ForceMode2D.Force);
+
+            if (_myRigidbody.velocity.x < MIN_FORCE)
+            {
+                _myRigidbody.velocity = new Vector2(MIN_FORCE, _myRigidbody.velocity.y);
+            }
+            else if (_myRigidbody.velocity.x > MAX_FORCE_NORMAL)
+            {
+                _myRigidbody.velocity = new Vector2(MAX_FORCE_NORMAL, _myRigidbody.velocity.y);
+
+            }
         }
         else
         {
-            
-            _myAnimator.SetBool("isRunning", false);
-            Debug.Log("I am standing still");
+            _myRigidbody.velocity = new Vector2(0, _myRigidbody.velocity.y);
+        }
+
+        if (movement.y > 0)
+        {
+            Jump();
         }
         
+        _myRigidbody.gravityScale =
+            _myRigidbody.velocity.y < 0 ? FALLING_GRAVITY_SCALE : GRAVITY_SCALE;
     }
+    private void Jump()
+    {
+        Debug.Log("Jump");
+        // If the player just started jumping, the jump height can be increased by holding jump button
+        if (_jumping && _currentJumpTime <= MaxJumpTime)
+        {
+            _myRigidbody
+                .AddForce(Vector2.up * Time.fixedDeltaTime * VerticalMoveSpeed / AdditionalJumpForce,
+                    ForceMode2D.Impulse);
+        }
 
+        if (!_touchesGround ||
+            _myRigidbody.velocity.y <
+            -0.001) // player cant jump if he doesnt touch the ground or is falling down
+        {
+            return;
+        }
+
+        _touchesGround = false;
+        _jumping = true;
+        _myRigidbody.AddForce(Vector2.up * Time.fixedDeltaTime * VerticalMoveSpeed, ForceMode2D.Impulse);
+    }
 }
